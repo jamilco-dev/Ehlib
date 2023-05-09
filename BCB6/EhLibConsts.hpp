1630288f76638c464e05109c99b3a4093eeaa8ec
devora8 <devora8@jam.local>
2023-05-09 11:28:18 +0300

    First commit

---
+++ BCB6/EhLibConsts.hpp
@@ -0,0 +1,62 @@
+// Borland C++ Builder
+// Copyright (c) 1995, 2002 by Borland Software Corporation
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'EhLibConsts.pas' rev: 6.00
+
+#ifndef EhLibConstsHPP
+#define EhLibConstsHPP
+
+#pragma delphiheader begin
+#pragma option push -w-
+#pragma option push -Vx
+#include <SysInit.hpp>	// Pascal unit
+#include <System.hpp>	// Pascal unit
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Ehlibconsts
+{
+//-- type declarations -------------------------------------------------------
+//-- var, const, procedure ---------------------------------------------------
+extern PACKAGE System::ResourceString _SClearSelectedCellsEh;
+#define Ehlibconsts_SClearSelectedCellsEh System::LoadResourceString(&Ehlibconsts::_SClearSelectedCellsEh)
+extern PACKAGE System::ResourceString _SInvalidTextFormatEh;
+#define Ehlibconsts_SInvalidTextFormatEh System::LoadResourceString(&Ehlibconsts::_SInvalidTextFormatEh)
+extern PACKAGE System::ResourceString _SInvalidVCLDBIFFormatEh;
+#define Ehlibconsts_SInvalidVCLDBIFFormatEh System::LoadResourceString(&Ehlibconsts::_SInvalidVCLDBIFFormatEh)
+extern PACKAGE System::ResourceString _SErrorDuringInsertValueEh;
+#define Ehlibconsts_SErrorDuringInsertValueEh System::LoadResourceString(&Ehlibconsts::_SErrorDuringInsertValueEh)
+extern PACKAGE System::ResourceString _SIgnoreErrorEh;
+#define Ehlibconsts_SIgnoreErrorEh System::LoadResourceString(&Ehlibconsts::_SIgnoreErrorEh)
+extern PACKAGE System::ResourceString _STabularInformationEh;
+#define Ehlibconsts_STabularInformationEh System::LoadResourceString(&Ehlibconsts::_STabularInformationEh)
+extern PACKAGE System::ResourceString _SPageOfPagesEh;
+#define Ehlibconsts_SPageOfPagesEh System::LoadResourceString(&Ehlibconsts::_SPageOfPagesEh)
+extern PACKAGE System::ResourceString _SPreviewEh;
+#define Ehlibconsts_SPreviewEh System::LoadResourceString(&Ehlibconsts::_SPreviewEh)
+extern PACKAGE System::ResourceString _SFieldNameNotFoundEh;
+#define Ehlibconsts_SFieldNameNotFoundEh System::LoadResourceString(&Ehlibconsts::_SFieldNameNotFoundEh)
+extern PACKAGE System::ResourceString _SFindDialogStringNotFoundMessageEh;
+#define Ehlibconsts_SFindDialogStringNotFoundMessageEh System::LoadResourceString(&Ehlibconsts::_SFindDialogStringNotFoundMessageEh)
+extern PACKAGE System::ResourceString _SSTFilterListItem_Empties;
+#define Ehlibconsts_SSTFilterListItem_Empties System::LoadResourceString(&Ehlibconsts::_SSTFilterListItem_Empties)
+extern PACKAGE System::ResourceString _SSTFilterListItem_NotEmpties;
+#define Ehlibconsts_SSTFilterListItem_NotEmpties System::LoadResourceString(&Ehlibconsts::_SSTFilterListItem_NotEmpties)
+extern PACKAGE System::ResourceString _SSTFilterListItem_All;
+#define Ehlibconsts_SSTFilterListItem_All System::LoadResourceString(&Ehlibconsts::_SSTFilterListItem_All)
+extern PACKAGE System::ResourceString _SSTFilterListItem_ClearFilter;
+#define Ehlibconsts_SSTFilterListItem_ClearFilter System::LoadResourceString(&Ehlibconsts::_SSTFilterListItem_ClearFilter)
+extern PACKAGE System::ResourceString _SSTFilterListItem_SortingByDescend;
+#define Ehlibconsts_SSTFilterListItem_SortingByDescend System::LoadResourceString(&Ehlibconsts::_SSTFilterListItem_SortingByDescend)
+extern PACKAGE System::ResourceString _SSTFilterListItem_SortingByAscend;
+#define Ehlibconsts_SSTFilterListItem_SortingByAscend System::LoadResourceString(&Ehlibconsts::_SSTFilterListItem_SortingByAscend)
+
+}	/* namespace Ehlibconsts */
+using namespace Ehlibconsts;
+#pragma option pop	// -w-
+#pragma option pop	// -Vx
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// EhLibConsts
