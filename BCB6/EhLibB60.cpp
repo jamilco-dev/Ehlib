35336fee06c705a94ef6d784efc109d633df561c
devora8 <devora8@jam.local>
2023-05-09 17:46:21 +0300

    .

--- BCB6/EhLibB60.cpp
+++ BCB6/EhLibB60.cpp
@@ -5,7 +5,6 @@
 USEFORMNS("CalculatorEh.pas", Calculatoreh, CalculatorEh);
 USEFORMNS("PrnDgDlg.pas", Prndgdlg, fPrnDBGridEHSetupDialog);
 USEFORMNS("PrvFrmEh.pas", Prvfrmeh, PreviewFormEh);
-USEFORMNS("DBGridEhFindDlgs.pas", Dbgridehfinddlgs, DBGridEhFindDlg);
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
